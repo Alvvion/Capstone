0e097115afde0652cbc48e714cd96aa7e69bdfce
Utkarsh <alvvion@gmail.com>
2024-03-04 10:24:28 +0530

    Cap

---
+++ text/cleaners.py
@@ -0,0 +1,10 @@
+import re
+from text.english import english_to_lazy_ipa, english_to_ipa2, english_to_lazy_ipa2
+
+
+def cjke_cleaners2(text):
+    text = re.sub(r'\[EN\](.*?)\[EN\]',
+                  lambda x: english_to_ipa2(x.group(1))+' ', text)
+    text = re.sub(r'\s+$', '', text)
+    text = re.sub(r'([^\.,!\?\-…~])$', r'\1.', text)
+    return text
